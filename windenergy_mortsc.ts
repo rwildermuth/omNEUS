8f56d5edc1ea1c63b207f1297a7f2146ba299b36
Robert P Wildermuth <rwildermuth@umassd.edu>
2020-10-27 14:31:26 -0400

    Add wind energy mortality scalar forcing file

---
+++ windenergy_mortsc.ts
@@ -0,0 +1,17 @@
+# Time series forcing for Atlantis NEUS v1 Wind energy lease impacts on benthos
+# This assumes that rewind=0 in the prm file and ts_on_hydro_time=0
+
+## COLUMNS 2
+##
+## COLUMN1.name time
+## COLUMN1.long_name Time
+## COLUMN1.units days since 1964-01-01 00:00:00 +10
+## COLUMN1.missing_value -999
+##
+## COLUMN2.name mortsc
+## COLUMN2.long_name External mortality scalar
+## COLUMN2.units mortality per day
+## COLUMN2.missing_value -999
+##
+0 1
+18261 1.0001369863
